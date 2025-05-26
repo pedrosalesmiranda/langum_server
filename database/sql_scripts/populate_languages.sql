7f954a28f76561ba0eb70070053ce7d56142622e
psm19 <psm19g@gmail.com>
2025-05-26 14:15:08 +0200

    adds populate_languages.sql

---
+++ database/sql_scripts/populate_languages.sql
@@ -0,0 +1,5 @@
+INSERT INTO Languages (languageEng, languageNative, languageEngTwoLetters) VALUES
+('portuguese', 'Português', 'pt'),
+('russian', 'Русский', 'ru'),
+('polish', 'Polski', 'pl'),
+('english', 'English', 'en');
